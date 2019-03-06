8af0e1df566e514142c96bcb787bc8e3ee7b9257
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-03-06 16:07:19 -0500

    Some more tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+describe("<Dashboard />", () => {
+  it("shows the controls and display", () => {
+    render(<Dashboard />);
+  });
+});
