78bfe44df42545eb2ff399c5f163b35269b53749
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-03-06 15:51:34 -0500

    Finished some tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+  it("cannot be closed or opened if it is locked", () => {
+    const { getByTestId } = render(<Controls closed={true} locked={true} />);
+    const close = getByTestId("lockbtn");
+    const lock = getByTestId("closebtn");
+
+    expect(close.disabled).toBe(false);
+    expect(lock.disabled).toBe(true);
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("checks default is unlocked and open", () => {
+    const { getByText } = render(<Display />);
+    const lock = getByText(/unlocked/i).textContent;
+    const close = getByText(/open/i).textContent;
+    expect(lock).toBeTruthy();
+    expect(close).toBeTruthy();
+  });
+});
